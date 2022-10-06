bed32640bb53be78d1802561f9a435e40f15f83b
rizaalsyt <rizalsyt017@gmail.com>
2022-10-06 08:56:54 +0700

    feactAPI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import ButtonSelect from './Button'
 
+
+
+
+// kalao bisa reusable component hanya menampilkan kumoulan elemet yang mana 
+// datanya yang di ambil / di umpan dari parent , jangan menggunakan fungsi disini, 
 class Card extends Component {
   render() {
     return (
-      <div>
-        <img src="
-        " alt="" />
-        <p></p>
+      <div className=" bg-stone-300 max-w-sm rounded overflow-hidden shadow-lg border-black">
+                <img className="pr-3 px-3 pt-3 " src= {`https://image.tmdb.org/t/p/w500${this.props.image}`} alt={this.props.title} />
+                <p className="text-center pt-2 text-lg font-bold "> {this.props.title}</p>
+                <ButtonSelect />
       </div>
     )
   }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import "../style/App.css"
 import Container from "../components/Layout"
+import axios  from "axios";
 import ButtonSelect from '../components/Button';
 import { faker } from "@faker-js/faker";
 import Loading from '../components/Loading';
-
+import Card from "../components/Card";
 
 
 class App extends Component {
@@ -21,25 +22,28 @@ class App extends Component {
   }
 
 
-  fecthData () {
-    // Loading
-    this.setState({loading: true});
-    // mapping card
-  let dataTemp = []
-   for (let i = 0; i < 10; i++) {
-    const temp = {
-      id : i + 1,
-      title : faker.name.fullName(),
-      image: "https://images.unsplash.com/photo-1524481905007-ea072534b820?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-    }
-    dataTemp.push(temp)
-   }
-   this.setState({datas: dataTemp})
-   setTimeout(() =>  {
-    this.setState({ loading: false});
-   },3000)
-
+  fetchData() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMBD_KEY}&page=${this.state.page}`
+      )
+      .then((res) => {
+        const { results } = res.data; // destructuring
+        // const results = res.data.results;
+        const newPage = this.state.page + 1;
+        const temp = [...this.state.datas];
+        temp.push(...results);
+        this.setState({ datas: temp, page: newPage });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
   }
+
+  
 
 
   render() {
@@ -51,23 +55,19 @@ class App extends Component {
                 <Loading key={item}/>
               )) 
             : this.state.datas.map((data) => (
-              <div className=" bg-stone-300 max-w-sm rounded overflow-hidden shadow-lg border-black"
+              <Card
+              key={data.id} image={data.poster_path} title={data.title} />
               
-              key={data.id}>
-                <img className="pr-3 px-3 pt-3 " src={data.image} alt={data.title} />
-                <p className="text-center pt-2 text-lg "> {data.title}</p>
-                <ButtonSelect />
-              </div>
             ))}
-              {}
               </div>
+              <ButtonSelect label="Load More" onClick={() => this.fetchData()} />
             </div>
             
         </Container>
         
 
-    )
-  }
+    );
+  } 
 }
 
 
